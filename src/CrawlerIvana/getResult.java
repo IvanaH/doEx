ed027b20b61a5c958bdc6f0f024b0ff27f5b229c
IvanaShM <ivana.shimin@gmail.com>
2016-06-20 20:38:33 +0800

    --To do. Find the info mapping the key from the response. 20160620

--- src/CrawlerIvana/getResult.java
+++ src/CrawlerIvana/getResult.java
@@ -4,10 +4,14 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 class getResult {
 	private String k;
+	private String RFile = null;
+	private String Resultf = "D:\\JavaL\\WorkSpace\\Practice\\src\\CrawlerIvana\\CResult.txt";
 	
 	void setKey(String keys){
 		Scanner s = new Scanner(System.in);
@@ -22,6 +26,7 @@ class getResult {
 	void getResponce(String sLink, char sType){
 		BufferedReader rd = null;
 		FileWriter fw = null;
+		RFile = "D:\\JavaL\\WorkSpace\\Practice\\src\\CrawlerIvana\\Result.txt";
 		
 		try{
 			URL url = new URL("http://"+sLink);
@@ -33,28 +38,43 @@ class getResult {
 			else
 				con.setRequestMethod("POST");
 			
-			con.setReadTimeout(15*1000);
+			con.setReadTimeout(20*1000);
 			con.connect();
 			
 			//read the output
 			if(con.getResponseCode() == 200)
-				rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				rd = new BufferedReader(new InputStreamReader(con.getInputStream(),"UTF-8"));
 			else
 				System.out.println("No response from Server.");
 			
-			fw = new FileWriter("D:\\JavaL\\WorkSpace\\Practice\\src\\CrawlerIvana\\Result.txt");
+			fw = new FileWriter(RFile);
 			
 			String line = null;
+			
+//			fw.append(line+"\r\n");
 		    while ((line = rd.readLine()) != null){
-		    	fw.append(line);
-		    }
-			
-			//To-do, find the title contain the key word, then write the followed href info in to the result file
-			
-			
+		    	fw.append(line+"\r\n");
+		    }			
 		}catch(IOException exc){
 			System.out.println("Error occurred when get responce: "+exc);
 		}
-		
 	}
+	
+	void getR(String RFlie, String Resultf, String key){
+		try(BufferedReader br = new BufferedReader (new InputStreamReader (new FileInputStream(RFile),"UTF-8"));
+				FileWriter fw = new FileWriter(Resultf)){
+			
+			  Pattern p=Pattern.compile(key);
+			  Matcher m=p.matcher(br.readLine());
+			  
+			  while(m.find()){
+			   
+			  }
+			
+		}catch(IOException exc){
+			System.out.println("Error occurred: "+exc);
+		}
+	
+	}
+
 }
