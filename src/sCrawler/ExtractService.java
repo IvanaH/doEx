a1f4b9c6e5f172848fdf4da5c79be5820f4c7500
IvanaShM <ivana.shimin@gmail.com>
2016-12-05 19:57:58 +0800

    learning about downloading response by URLConnect. Still don't know httpclient. Now is processing the response to obtain info in pattern.

--- src/sCrawler/ExtractService.java
+++ src/sCrawler/ExtractService.java
@@ -1,16 +1,24 @@
 package sCrawler;
 
 import java.io.BufferedReader;
+import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.Connection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.lang.model.util.Elements;
 import javax.swing.text.Document;
+import javax.xml.ws.Response;
+
+import org.omg.CORBA.portable.ResponseHandler;
 
 
 class ExtractService {
@@ -20,6 +28,15 @@ class ExtractService {
 		LinkData data = null;
 		
 		try{
+//			//HttpClient
+//			URI u = new URI(rule.getUrl());
+//			DefaultHttpClient httpclient = new DefaultHttpClient();
+//			HttpGet httpget = new HttpGet(u);
+//			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+//			String content = httpclient.execute(httpget, responseHandler);
+//			content = new String(content.getBytes("ISO-8859-1"),"UTF-8");
+//			return content;
+			
 			//analyzing rule
 			URL url2 = new URL(rule.getUrl());
 			String[] params = rule.getParams();
@@ -27,45 +44,39 @@ class ExtractService {
 			String resultTagName = rule.getResultTagName();
 			int requestType = rule.getRequestType();
 			
-			Connection con =  url2.openConnection();
+			URLConnection con =  url2.openConnection();
 			
 			con.setReadTimeout(20*1000);
 			con.connect();
 			
-			if(params != null){
-				for(int i =0; i< params.length;i++){
-					//
-				}
-			}
-			
-			Document doc = null;
-			doc = con.getURL();
-			
 			//read the output
-//			BufferedReader rd = null;
-//			FileWriter fw = null;
-//			
-//			if(con.getResponseCode() == 200)
-//				rd = new BufferedReader(new InputStreamReader(con.getInputStream(),"UTF-8"),1 * 1024 * 1024);
-//			else
-//				System.out.println("No response from Server.");
-//			
-////			fw = new BufferedWriter(new OutputStreamWriter(RFile),1 * 1024 * 1024);
-//			fw = new FileWriter(RFile);
-//			
-//			String line = null;
-//	    	line = rd.readLine();
-//			
-//		    while (line != null){
-//		    	fw.append(line+"\r\n");
-////		    	fw.write(line+"\r\n");
-//		    	
-//		    	line = rd.readLine();
-//		    }		
-//		    
-//		    fw.flush();
+			BufferedReader rd = null;
+		
+			rd = new BufferedReader(new InputStreamReader(con.getInputStream(),"UTF-8"),1 * 1024 * 1024);
+			String html = "";
+			String line = rd.readLine();
 			
-
+		    while (line != null){
+		    	html = html + line;
+		    }
+		    
+		    //process output of the link
+		    String reg = "<h1 class=\"headline-title\">(.*?)<h1>";
+			Pattern P = Pattern.compile(reg);
+			Matcher m = P.matcher(html);
+			  
+			  while(m.find()){
+				  i++;
+				  System.out.print("Find "+i + ": ");
+				  if(m.group(1).contains(key)){
+					  System.out.println("Match! ");
+//					  System.out.println(m.group(1));
+					  fw.append(m.group(1));
+				      fw.append(": "+"http://daily.zhihu.com"+m.group(3)+"\r\n");
+				  }
+				  else
+					  System.out.println("Not match. ");
+			  }
 
 		}catch (IOException e){
 			e.printStackTrace();
